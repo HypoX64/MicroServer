6546a01a33e0007eb881b38407e69333ebe35056
Haibo Lei <834201093@qq.com>
2018-06-12 22:34:38 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MicroServer
 A micro server built in python.(使用python搭建的微型服务器)
+
+# Test
+* 注册json格式：{"mod":"register","account":"hypo","password":"123456"}
+* 登录json格式：{"mod":"login","account":"hypo","password":"123456"}
+* 数据发送：{"mod":"data"}
--- server.py
+++ server.py
@@ -1,57 +1,104 @@
+#encoding = utf-8#
 from socket import *
 import json
+import threading
+import datetime
 
-address='172.16.252.183'     #监听哪些网络  127.0.0.1是监听本机 0.0.0.0是监听整个网络
+address='172.16.252.183'  
+# address='192.168.191.137'     #监听哪些网络  127.0.0.1是监听本机 0.0.0.0是监听整个网络
 port=20000           #监听自己的哪个端口
 buffsize=1024          #接收从客户端发来的数据的缓存区大小
-user = {'hypo':'123456'}
+database = './database'
+logpath = './log'
+user = {}
 
-
-def loadjson(recvjsonData):
-    replyData={}
-    try:
-        recvdata=json.loads(recvjsonData)
-        # print(recvdata)
-        # print(recvdata["password"])
-
-        if recvdata['mod']=='register':
-            if recvdata['account'] in user:
-                replyData['reply']='account has exised'
-            else :
-                user[recvdata['account']]=recvdata['password']
-                replyData['reply']='register succeed'
-
-        if recvdata['mod']=='login':
-            if recvdata['account'] in user:
-                if user[recvdata['account']]==recvdata['password']:
-                    replyData['reply']='login succeed'
-                else:
-                    replyData['reply']='password wrong'
-            else:
-                replyData['reply']='account not exise'
-
-
-        return(replyData)
-
-    except Exception as e:
-        print('error:',e)
-
-print("run!")
 s = socket(AF_INET, SOCK_STREAM)
 s.bind((address,port))
-s.listen(1)     #最大连接数
-while True:
+s.listen(10)     #最大连接数
+
+def loaddatabase(path):
+    data={}
+    for line in open(path):
+        line=line.strip()
+        data=json.loads(line)
+    return data
+
+def writelog(log):
+    f = open(logpath,"a+")
+    f.write(log+'\n')
+    print(log)
+
+def loadjson(recvjsonData):
+    global user
+    replydata={}
     try:
-        clientsock,clientaddress=s.accept()
-        print('connect from:',clientaddress)
-    #传输数据都利用clientsock，和s无关
-        while True:  
-            recvdata=clientsock.recv(buffsize).decode('utf-8')
-            if recvdata=='exit' or not recvdata:
-                break
-            senddata=json.dumps(loadjson(recvdata))
-            clientsock.send(senddata.encode())
-        clientsock.close()
+        recvdata=json.loads(recvjsonData)
+
+        #register
+        if recvdata['mod']=='register':
+            if recvdata['account'] in user:
+                replydata['reply']='account has existed'
+            else :
+                user[recvdata['account']]=recvdata['password']
+                f = open(database,"w+")
+                f.write(str(json.dumps(user)))
+                replydata['reply']='ok'
+            writelog(recvdata['mod']+': '+recvdata['account']+': '+replydata['reply'])
+
+        #login
+        elif recvdata['mod']=='login':
+            if recvdata['account'] in user:
+                if user[recvdata['account']]==recvdata['password']:
+                    replydata['reply']='ok'
+                else:
+                    replydata['reply']='wrong password'
+            else:
+                replydata['reply']='account not exist'
+            writelog(recvdata['mod']+': '+recvdata['account']+': '+replydata['reply'])
+        
+        #data analysis
+        elif recvdata['mod']=='data':
+            replydata['reply']='data!'
+            writelog(recvdata['mod']+': '+replydata['reply'])
+
+        #error
+        else:
+            replydata['reply']='error: please send data as example'
+
+        return replydata
+
     except Exception as e:
-        print('error:',e)
-s.close()
+        writelog('error:'+str(e))
+        # print(datetime.datetime.now(),'error:',e)
+        replydata['reply']='error: please send as example'
+        return replydata 
+
+
+def tcplink(clientsock):
+    # while True: 
+    recvdata=clientsock.recv(buffsize).decode('utf-8')
+    if recvdata=='exit' or not recvdata:
+        clientsock.close()
+        return
+    senddata=json.dumps(loadjson(recvdata))
+    clientsock.send(senddata.encode())
+    clientsock.close()
+
+def main(): 
+    global user
+    user=loaddatabase(database)
+    writelog('\n'+str(datetime.datetime.now())+"\nserver run!")
+    while True:
+        try:
+            clientsock,clientaddress=s.accept()
+            writelog(str(datetime.datetime.now())+' connect from:'+str(clientaddress))
+            # print(datetime.datetime.now(),'connect from:',clientaddress)
+            t=threading.Thread(target=tcplink,args=(clientsock,))  #t为新创建的线程
+            t.start()
+        except Exception as e:
+            writelog(str(datetime.datetime.now())+' error:'+str(e))
+            # print(datetime.datetime.now(),'error:',e)
+    s.close()
+
+if __name__ == '__main__':
+    main()
